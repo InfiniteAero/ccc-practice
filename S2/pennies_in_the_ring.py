371793a42dcbdeedd2de8926f49375ed23efcdc3
Edwin Tran <edwinachieve@gmail.com>
2024-09-20 13:28:16 -0400

    Completed CCC 08 S2

---
+++ S2/pennies_in_the_ring.py
@@ -0,0 +1,18 @@
+# this is the most big-brain code I've ever written
+# took five minutes to think up anyway
+import math
+
+list_points = []
+while True:
+    r = int(input())
+    points = 1
+    if r != 0:
+        for i in range(-r, r):
+            y = math.floor(math.sqrt((r ** 2) - (abs(i) ** 2)))
+            points += y * 2 + 1
+    else: break
+    list_points.append(points)
+
+for point in list_points:
+    print(point)
+    
